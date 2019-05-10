8624d8e5e153739b01704faa2ece1e320ec19f20
Littlewolfwork <36741698+Littlewolfwork@users.noreply.github.com>
2019-05-10 20:43:26 +0500

    version 1.1
    Добавил комментарии, мелкие исправления для стандарта оформления кода

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// функция для перемешивания элементов массива
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
@@ -5,10 +6,13 @@ function shuffle(array) {
     }
 }
 
+// массив для хранения карты цветов клеток
 let indexColors = [];
+
+// промежуточный массив для обеспесения случайности пар цветов
 let randColors = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 shuffle(randColors);
-let q=0;
+let q = 0;
 for (let i = 0; i < 4; i++){
     let tempColors = [];
     for (let j = 0; j < 4; j++){
@@ -18,9 +22,14 @@ for (let i = 0; i < 4; i++){
     indexColors.push(tempColors);
 }
 
+// выводит секунды, минуты и миллисекунды с нужным количеством нулей
 function add0 (x, y) {let s = '00' + x; return s.substr (s.length - y)}
+
+// переменные для обслуживания секундомера
 let RAF;
 let startTime;
+
+// функция, реализующая тик секндомера и изменение отображаемых цифр
 function Tick ()
 {
     let ms = Date.now() - startTime;
@@ -28,32 +37,38 @@ function Tick ()
     ms = ms % 1000;
     let M = Math.floor(S / 60);
     S = S % 60;
+
+    // TODO: переделать для соответствия стандарту оформления кода
     document.querySelector("#timer").textContent = [add0(M, 2), add0(S, 2), add0(ms, 3)].join (':');
     RAF = requestAnimationFrame(Tick);
 }
 
-console.log(indexColors);
-console.log(indexColors[0][0]);
+//обработчик кнопки старт
 let btn = document.querySelector('#btn');
 btn.addEventListener("click", function(){
     startTime = Date.now();
     Tick();
+    // пропишем ячейкам таблицы обработчик клика
+    let arrayTd = document.querySelectorAll('td');
+    for (let i = 0; i < arrayTd.length; i++) {
+        arrayTd[i].addEventListener('click', clickHandler);
+    }
 
 
+    // TODO: перенести генерацию карты клеток в эту функцию
 });
 
-let arrayTd = document.querySelectorAll('td');
 
-for (let i = 0; i < arrayTd.length; i++) {
-    arrayTd[i].addEventListener('click', clickHandler);
-}
 
 function clickHandler(event) {
     event.preventDefault();
+
+    // проверка, не является ли кликнутая клетка уже открытой или из разблокированной пары
     if (this.classList.contains('stable')||(this.classList.contains('open'))){
         return;
     }
     else{
+
         let cellIndex = this.cellIndex;
         let rowIndex = this.parentNode.rowIndex;
         if (document.querySelectorAll(".open").length>0){
@@ -75,6 +90,8 @@ function clickHandler(event) {
                 console.log("color not ok");
                 this.classList.add("color"+indexColors[rowIndex][cellIndex]);
                 let currentCell = this;
+
+                // таймаут для задержки, чтоб пользователь увидел как прорисовывается и пропадает неправильная пара цветов
                 setTimeout(function(){
                     currentCell.classList.remove("color"+indexColors[rowIndex][cellIndex]);
                     openCell.classList.remove("open");
@@ -84,7 +101,7 @@ function clickHandler(event) {
         }
         else{
             this.classList.add("open");
-            const str="color"+indexColors[rowIndex][cellIndex];
+            const str = "color"+indexColors[rowIndex][cellIndex];
             this.classList.add("color"+indexColors[rowIndex][cellIndex]);
         }
     }
